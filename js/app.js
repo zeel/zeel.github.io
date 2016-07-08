61e1562b2ab39570d9b3140da818b79ddcc2a423
zeel shah <zeel.shah@sprinklr.com>
2016-07-09 01:55:59 +0530

    added react flux

--- js/app.js
+++
@@ -1,6 +0,0 @@
-define(['angular', 'todoController'],function(angular, todoController)
-{
-	var app = angular.module('todoApp', []);
-	app.controller('todoController', todoController);
-	return app;
-})
