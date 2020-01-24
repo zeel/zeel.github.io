190f3b6a6a0752825c9fc90249e9413a6e6379d6
zeel <kshah215@gmail.com>
2020-01-24 19:23:38 +0530

    fix: added pics and removed  others

---
+++ pics/precache-manifest.9328bfd59172389d83e3cd0aeadddeb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a803af1be3f0411d9f51ef47840bff9",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "c9b056f495ef88e90ba1",
+    "url": "/pics/static/css/main.d4b1252e.chunk.css"
+  },
+  {
+    "revision": "a6a99c0c7bbdea79f27f",
+    "url": "/pics/static/js/2.d5a02f09.chunk.js"
+  },
+  {
+    "revision": "c9b056f495ef88e90ba1",
+    "url": "/pics/static/js/main.bedb27ff.chunk.js"
+  },
+  {
+    "revision": "9198ae3b868f769fc8ea",
+    "url": "/pics/static/js/runtime~main.7218180c.js"
+  }
+]);
